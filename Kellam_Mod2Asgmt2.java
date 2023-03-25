e925fa098ae4d26e80cc37a53e98c9519016cb39
JK <jkellam@my365.bellevue.edu>
2023-03-25 11:45:16 -0500

    320 Module 2 assignment

--- Kellam_Mod2Asgmt2.java
+++ Kellam_Mod2Asgmt2.java
@@ -31,11 +31,13 @@ public class Kellam_Mod2Asgmt2 {
         System.out.println("Hello! We will calculate the energy, in Joules,");
         System.out.println("that is required for the water sample to reach");
         System.out.println("your desired temperature, in degrees Celsius.");
-        System.out.println("Please enter the initial temperature of the water, in degrees Celsius:");
+        System.out.println("Please enter the initial temperature of the water,");
+        System.out.println("in degrees Celsius:");
         Scanner scanner = new Scanner(System.in);
         double initialTemperature = scanner.nextDouble();
         System.out.println("You have entered " + initialTemperature + " degrees Celsius.");
-        System.out.println("Please enter the final temperature that you want the water to achieve, in degrees Celsius");
+        System.out.println("Please enter the final temperature that you want the water to achieve,");
+        System.out.println("in degrees Celsius");
         double finalTemperature = scanner.nextDouble();
         System.out.println("You have entered " + finalTemperature + " degrees Celsius as your target temperature.");
         System.out.println("Please enter the mass of the water, in Kilograms");

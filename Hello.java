e45b5012d08ba1acede0f853acb3d40ce2e89dcd
JK <jkellam@my365.bellevue.edu>
2023-04-11 22:43:07 -0500

    Push #2 4/11/2023

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello,this is a beginnning file in VS Code to start Java.");
+        System.out.println("Hello,this is a beginning file in VS Code to start Java.");
     }
 }

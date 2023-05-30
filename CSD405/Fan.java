db2f4d1129508879b8dab6685f280e15c85304bd
JK <jkellam@my365.bellevue.edu>
2023-05-30 14:52:53 -0500

    Reorganized java folder

---
+++ CSD405/Fan.java
@@ -0,0 +1,26 @@
+/*
+ * CSD 405
+ * Module 1 Assignment
+ * Jeremiah Kellam
+ * 5/30/2023
+ * 
+ * Write a JAVA program with a class titled Fan. This class is to contain:
+ *     Four constants named STOPPED, SLOW, MEDIUM, and FAST. The constants are to hold the values of 0, 1, 2, and 3 respectively.
+ *     A private field named speed that holds one of the constant values with the default being STOPPED.
+ *     A private Boolean field titled on that specifies whether the fan is on or off.
+ *     A private field named radius that holds the radius of the fan with a default value of 6.
+ *     A String field that holds the color, with the default being white.
+ *     Setter and getter methods for all mutable fields.
+ *     A no-argument constructor that sets all fields with a default value.
+ *     A constructor taking arguments and setting values.
+ *     Write a toString() method that returns a description of the Fans state.
+ *     Write test code that creates two instances of the Fan class, one using the default constructor and the other using the argument constructor. Write code that displays the functionality of the Fan class methods.
+ */
+
+ public class Fan{
+    public static final int STOPPED = 0;
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+    
+ }
